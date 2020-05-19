2bafca05545b85bd632c740d5e9c24e987448c95
JonJakova <50449118+JonJakova@users.noreply.github.com>
2020-05-19 14:09:09 +0200

    fixed db id generation bug

--- scheduler/db_updater.js
+++ scheduler/db_updater.js
@@ -1,9 +1,10 @@
 const fetch = require('node-fetch');
 
 const db_updater = (db) => {
-    fetch(_upcomingURL)
-        .then(res => res.json())
-        .then(json => loopInsert(db, json.results))
+  fetch(_upcomingURL)
+      .then(res => res.json())
+      .then(json => checkForSame(db, json.results))
+      .catch(err => console.log(err))
 }
 
 function generateQuery(db, showData) {
@@ -24,12 +25,37 @@ function generateQuery(db, showData) {
     .catch((err) => console.log(err));
 }
 
-function loopInsert(db, showArray) {
-    showArray.forEach((show, i) => {
-        generateQuery(db, show);
-        // getSeason(showArray[i].release_date)
-    });
-    // generateQuery(db, showArray[0]);
+//showlist is an array of objects that contains movies data
+function checkForSame(db, showList) {
+  const obj = {};
+
+  db.select('tmdb_id').from('shows')
+    .then(data => {
+      let dataForDB = []
+
+      for(let i = 0; i < data.length; i++){
+        if(!obj[data[i].tmdb_id]){
+          const element = data[i].tmdb_id;
+          obj[element] = true;
+        }
+      }
+
+      for (let j = 0; j < showList.length; j++) {
+        if(!obj[showList[j].id]){
+          dataForDB.push(showList[j]);
+        }
+      }
+
+      return dataForDB;
+    })
+    .then(dtForDB => loopInsert(db, dtForDB))
+    .catch( (err) => console.log(err))
+}
+
+function loopInsert(db, dataForDB) {
+  dataForDB.forEach(show => {
+    generateQuery(db, show);
+  })
 }
 
 function getSeason(date){
